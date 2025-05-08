0f053cce2e55c18c263cadd51f556f618155b024
nixuris <nixuriss@proton.me>
2025-05-08 22:41:03 +0700

    Nix Develop

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "configuration.nix + home.nix in one flake";
+  description = "configuration.nix + home.nix in one flake with dev shell";
   inputs = {
     nixpkgs.url    = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
@@ -10,7 +10,44 @@
   outputs = { self, nixpkgs, home-manager, ... }:
   let
     system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
   in {
+    # Development shell for project
+    devShells = {
+      x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.git-filter-repo
+          pkgs.docker
+          pkgs.gcc
+          pkgs.gdb
+          pkgs.clang-tools
+          pkgs.cmake
+          pkgs.ninja
+          pkgs.jdk
+          pkgs.nodejs_23
+          pkgs.pnpm
+          pkgs.nodePackages.eslint
+          pkgs.nodePackages.prettier
+          (pkgs.python3.withPackages (ps: with ps; [ pip virtualenv ]))
+        ];
+
+        shellHook = ''
+          LIBRARY_PATH="${pkgs.stdenv.cc.cc}/lib:${pkgs.glibc}/lib:${pkgs.libgcc}/lib:${pkgs.libclang}/lib"
+          export NIX_LD_LIBRARY_PATH="$LIBRARY_PATH"
+          export NIX_LD="${pkgs.stdenv.cc}/nix-support/dynamic-linker"
+
+          if [ ! -d .venv ]; then
+            python -m venv .venv
+          fi
+          export VIRTUAL_ENV_DISABLE_PROMPT=1
+          source .venv/bin/activate
+          echo "Dev environment (Java, JS, C/C++, Python) is ready!"
+          exec fish
+        '';
+      };
+    };
+
+    # NixOS configuration
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         inherit system;
@@ -25,88 +62,57 @@
               isNormalUser = true;
               extraGroups = [ "networkmanager" "wheel" "libvirtd" "kvm" "adbusers" ];
             };
-              #Tablet
-              #hardware.opentabletdriver.enable = true;
           })
-
-          # Home Manager
           home-manager.nixosModules.home-manager
           ({ config, pkgs, ... }: {
-            home-manager = {
-              useGlobalPkgs    = true;
-              users.nixuris = {
-                imports = [
-                  ./modules/home/alacritty.nix
-                  ./modules/home/cava.nix
-                  ./modules/home/fastfetch.nix
-		              ./modules/home/fish.nix
-		              ./modules/home/hypr.nix
-		              ./modules/home/nvim.nix
-		              ./modules/home/ranger.nix
-		              ./modules/home/rofi.nix
-		              ./modules/home/swappy.nix
-		              ./modules/home/swaylock.nix
-		              ./modules/home/swaync.nix
-		              ./modules/home/waybar.nix
+            home-manager.useGlobalPkgs = true;
+            home-manager.users.nixuris = {
+              imports = [
+                ./modules/home/alacritty.nix
+                ./modules/home/cava.nix
+                ./modules/home/fastfetch.nix
+                ./modules/home/fish.nix
+                ./modules/home/hypr.nix
+                ./modules/home/nvim.nix
+                ./modules/home/ranger.nix
+                ./modules/home/rofi.nix
+                ./modules/home/swappy.nix
+                ./modules/home/swaylock.nix
+                ./modules/home/swaync.nix
+                ./modules/home/waybar.nix
+              ];
+              i18n = {
+                inputMethod.enable = true;
+                inputMethod.type   = "fcitx5";
+                inputMethod.fcitx5 = {
+                  waylandFrontend = true;
+                  addons          = with pkgs; [ fcitx5-unikey ];
+                };
+              };
+              home = {
+                username      = "nixuris";
+                homeDirectory = "/home/nixuris";
+                stateVersion  = "25.05";
+                packages      = with pkgs; [
+                  dconf vscode firefox telegram-desktop vesktop mpv ani-cli
+                  cmus playerctl imv libreoffice-fresh zoom-us obs-studio
+                  obs-studio-plugins.wlrobs
                 ];
-
-                i18n.inputMethod = {
-                  enable = true;
-                  type   = "fcitx5";
-                  fcitx5 = {
-                    waylandFrontend = true;
-                    addons          = with pkgs; [ fcitx5-unikey ];
-                  };
+                sessionVariables = {
+                  EDITOR         = "nvim";
+                  VISUAL         = "nvim";
+                  XDG_CONFIG_HOME = "$HOME/.config";
+                  NIXOS_OZONE_WL = "1";
                 };
-
-                home = {
-                  username       = "nixuris";
-                  homeDirectory  = "/home/nixuris";
-                  stateVersion   = "25.05";
-                  packages       = with pkgs; [ 
-                    dconf
-                    vscode #code-cursor 
-                    #obsidian
-    
-                    # Media
-                    firefox telegram-desktop vesktop
-                    mpv ani-cli
-                    cmus playerctl #youtube-music ffmpeg yt-dlp 
-                    imv
-                    libreoffice-fresh
-    
-                    #Others  
-                    zoom-us
-                    obs-studio obs-studio-plugins.wlrobs #obs wayland
-                    #cbonsai cmatrix pipes tty-clock #cli aesthetics
-                  ];
-                  sessionVariables = {
-                    EDITOR          = "nvim";
-                    VISUAL          = "nvim";
-                    XDG_CONFIG_HOME = "$HOME/.config";
-                    NIXOS_OZONE_WL  = "1";
-                  };
-                };
-
-                gtk = {
-                  enable      = true;
-                  cursorTheme = {
-                    package = pkgs.bibata-cursors;
-                    name    = "Bibata-Modern-Ice";
-                  };
-                  theme = {
-                    package = pkgs.catppuccin-gtk;
-                    name    = "catppuccin-frappe-blue-standard";
-                  };
-                  iconTheme = {
-                    package = pkgs.papirus-icon-theme;
-                    name    = "Papirus-Dark";
-                  };
-                };
+              };
+              gtk = {
+                enable      = true;
+                cursorTheme = { package = pkgs.bibata-cursors; name = "Bibata-Modern-Ice"; };
+                theme       = { package = pkgs.catppuccin-gtk; name = "catppuccin-frappe-blue-standard"; };
+                iconTheme   = { package = pkgs.papirus-icon-theme; name = "Papirus-Dark"; };
               };
             };
           })
-
         ];
       };
     };
