9bf05423546d0b12bb8982cd35177d5d6b4bbb32
nixuris <nixurisu@gmail.com>
2025-05-11 00:13:18 +0700

    Power management

--- modules/power.nix
+++ modules/power.nix
@@ -5,39 +5,50 @@
     services.tlp = {
       enable = true;
       settings = {
-      	DISK_IDLE_SECS_ON_AC=0;
-      	DISK_IDLE_SECS_ON_BAT=60;
+      	DISK_IDLE_SECS_ON_AC=30;
+      	DISK_IDLE_SECS_ON_BAT=30;
 
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        #CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
 	
 	      CPU_BOOST_ON_AC=1;
 	      CPU_BOOST_ON_BAT=0;
-	
-	      PLATFORM_PROFILE_ON_AC= "performance";
-	      PLATFORM_PROFILE_ON_BAT= "low-power";
+	      CPU_HWP_DYN_BOOST_ON_AC=1;
+        CPU_HWP_DYN_BOOST_ON_BAT=0;
+
+	      #PLATFORM_PROFILE_ON_AC= "performance";
+	      PLATFORM_PROFILE_ON_AC= "balanced";
+	      PLATFORM_PROFILE_ON_BAT= "quiet";
 	
 	      WIFI_PWR_ON_AC= "off";
 	      WIFI_PWR_ON_BAT= "on";
 	
-	      PCIE_ASPM_ON_AC= "default";
-	      PCIE_ASPM_ON_BAT= "powersave";
+	      PCIE_ASPM_ON_AC= "powersupersave";
+	      PCIE_ASPM_ON_BAT= "powersupersave";
 
-	      RUNTIME_PM_ON_AC= "on";
+	      RUNTIME_PM_ON_AC= "auto";
 	      RUNTIME_PM_ON_BAT= "auto";
 
 	      SOUND_POWER_SAVE_ON_AC=0;
 	      SOUND_POWER_SAVE_ON_BAT=10;
+        
+        START_CHARGE_THRESH_BAT0=85;
+        STOP_CHARGE_THRESH_BAT0=90;
 
 	      USB_AUTOSUSPEND=1;
-
-	      CPU_SCALING_MIN_FREQ_ON_AC=400000;
-	      CPU_SCALING_MAX_FREQ_ON_AC=2300000;
-	      CPU_SCALING_MIN_FREQ_ON_BAT=400000;
-	      CPU_SCALING_MAX_FREQ_ON_BAT=100000;
+        
+        CPU_MIN_PERF_ON_AC=0;
+        CPU_MAX_PERF_ON_AC=100;
+        CPU_MIN_PERF_ON_BAT=0;
+        CPU_MAX_PERF_ON_BAT=50;
+	      #CPU_SCALING_MIN_FREQ_ON_AC=400000;
+	      #CPU_SCALING_MAX_FREQ_ON_AC=4600000;
+	      #CPU_SCALING_MIN_FREQ_ON_BAT=400000;
+	      #CPU_SCALING_MAX_FREQ_ON_BAT=100000;
     }; 
   };
 }
