7fe3c63804a3b03d6db1cc3ff94a66cd008b2586
nixuris <nixuriss@proton.me>
2025-05-16 00:41:45 +0700

    nvidia launch alias

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -37,6 +37,7 @@
       xu = "nix flake update";
       xd = "nix develop ~/hyprnix#default";
       e = "nvim";
+      gpu = "__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia";
     };
     interactiveShellInit = ''
       fastfetch
