bca8c0a3a38688cb8bf5ca0aecdbfe46bbb9306b
nixuris <nixuriss@proton.me>
2025-05-27 14:02:38 +0700

    Slight power tweak

--- modules/power.nix
+++ modules/power.nix
@@ -13,10 +13,10 @@
       DISK_IDLE_SECS_ON_BAT = 30;
 
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      #CPU_SCALING_GOVERNOR_ON_AC = "powersave";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      #CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_performance";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
       CPU_BOOST_ON_AC = 1;
