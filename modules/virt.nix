08ce57d89bf94983514d99788ded9fb6f6098f3e
nixuris <nixuriss@proton.me>
2025-05-29 17:59:20 +0700

    GPU Passthrough (still error 43)

--- modules/virt.nix
+++ modules/virt.nix
@@ -1,12 +1,22 @@
 {
   config,
-  lib,
   pkgs,
+  lib,
   ...
-}: {
-  virtualisation.libvirtd.enable = true;
-  programs.virt-manager.enable = true;
-  services.qemuGuest.enable = true;
-  services.spice-vdagentd.enable = true; # enable copy and paste between host and guest
-  virtualisation.spiceUSBRedirection.enable = true;
+}:{
+  boot.kernelParams = [
+    "intel_iommu=on"
+    "iommu=pt"
+    "vfio-pci.ids=10de:28e1,10de:22be"
+  ];
+  boot.initrd.kernelModules = [ "vfio_pci" "vfio" "vfio_iommu_type1" "i915"];
+  config = {
+    # ─── LIBVIRTD & QEMU ─────────────────────────────────────────
+    virtualisation.libvirtd.enable = true;
+    virtualisation.libvirtd.qemu.package = pkgs.qemu_kvm;
+    programs.virt-manager.enable = true;
+    services.qemuGuest.enable = true;
+    services.spice-vdagentd.enable = true; # enable copy and paste between host and guest
+    virtualisation.spiceUSBRedirection.enable = true; 
+  };
 }
