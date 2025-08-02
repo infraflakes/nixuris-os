1ca6a30fbc4537703e62ebb99ea2016e1b36a0cc
nixuris <nixuriss@proton.me>
2025-08-02 18:14:52 +0700

    Fix uptime not showing on powermenu

--- home/serein/shared/rofi.nix
+++ home/serein/shared/rofi.nix
@@ -4,7 +4,7 @@
   pkgs,
   ...
 }: {
-  home.packages = with pkgs; [ wl-clipboard ];
+  home.packages = with pkgs; [ wl-clipboard procps ];
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
