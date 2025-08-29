c2a7ec9897c9a185f3a9ddf553fb2a841dbab270
nixuris <nixuriss@proton.me>
2025-08-29 23:02:51 +0700

    Fix mpd startup

--- home/serein/niri/niri.nix
+++ home/serein/niri/niri.nix
@@ -150,7 +150,7 @@
 
       spawn-at-startup "fcitx5"
       spawn-at-startup "udiskie"
-      spawn-at-startup "mpd && mpd-mpris"
+      spawn-at-startup "mpd ~/.config/mpd/mpd.conf && mpd-mpris"
       spawn-at-startup "mpris-discord-rpc"
 
       window-rule {
