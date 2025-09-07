fd120a8ab4d7e8bed48e5e19f0f1a5b35dd7256f
nixuris <nixuriss@proton.me>
2025-09-07 16:09:25 +0700

    Fix wrong editor yazi spawn, extra ghostty keybind

--- home/confs/ghostty.nix
+++ home/confs/ghostty.nix
@@ -11,6 +11,8 @@
         "ctrl+t=new_tab"
         "ctrl+a=toggle_tab_overview"
         "ctrl+q=close_tab"
+        "ctrl+right=next_tab"
+        "ctrl+left=previous_tab"
       ];
       font-family = "JetBrains Mono Nerd Font";
       font-size = 13;
