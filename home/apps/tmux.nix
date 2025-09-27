0563b620fe8cb361cfe02f77c3829b1a49981aa6
nixuris <nixuriss@proton.me>
2025-09-27 12:30:32 +0700

    Personal tmux config (paired with nvim)

--- home/apps/tmux.nix
+++ home/apps/tmux.nix
@@ -32,6 +32,24 @@
     extraConfig = ''
       set -g renumber-windows on
       set-option -g allow-rename off
+
+      # ----- begin custom no-prefix binds -----
+
+      bind -n C-Left  switch-client -p
+      bind -n C-Right switch-client -n
+
+      bind -n M-\\ split-window -h
+      bind -n M-- split-window -v
+      bind -n M-y copy-mode
+      bind -n M-x kill-pane
+      bind -n M-a choose-tree -s
+      bind -n M-n new-window
+      bind -n M-q detach-client
+      bind -n M-Left  previous-window
+      bind -n M-Right next-window
+
+      bind -n C-r command-prompt -I '#S' 'rename-session -- "%%"'   # session
+      bind -n M-r command-prompt -I '#W' 'rename-window "%%"' 	#window
     '';
   };
   home.packages = with pkgs; [
