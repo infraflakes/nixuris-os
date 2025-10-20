555dda5639bd8306448f53229428d71c234bb3e2
nixuris <nixuriss@proton.me>
2025-10-21 00:26:36 +0700

    C

--- flake.nix
+++ flake.nix
@@ -15,36 +15,40 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    serein-cli,
-    zen-browser,
-    ...
-  } @ inputs: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-  in {
-    # Formatter
-    formatter.${system} = pkgs.nixfmt-rfc-style;
-    # NixOS configuration
-    nixosConfigurations.serein = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs system;};
-      modules = [./syswide/host.nix];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      serein-cli,
+      zen-browser,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      # Formatter
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+      # NixOS configuration
+      nixosConfigurations.serein = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs system; };
+        modules = [ ./syswide/host.nix ];
+      };
+      # Home Manager
+      homeConfigurations."nixuris@serein" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = { inherit inputs; };
+        modules = [ ./home/home.nix ];
+      };
+      # Dev Shell
+      devShells.${system} = {
+        rs = import ./devshells/rs.nix { inherit pkgs; };
+        go = import ./devshells/go.nix { inherit pkgs; };
+        js = import ./devshells/js.nix { inherit pkgs; };
+        py = import ./devshells/py.nix { inherit pkgs; };
+        c = import ./devshells/c.nix { inherit pkgs; };
+      };
     };
-    # Home Manager
-    homeConfigurations."nixuris@serein" = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      extraSpecialArgs = {inherit inputs;};
-      modules = [./home/home.nix];
-    };
-    # Dev Shell
-    devShells.${system} = {
-      rs = import ./devshells/rs.nix {inherit pkgs;};
-      go = import ./devshells/go.nix {inherit pkgs;};
-      js = import ./devshells/js.nix {inherit pkgs;};
-      py = import ./devshells/py.nix {inherit pkgs;};
-    };
-  };
 }
