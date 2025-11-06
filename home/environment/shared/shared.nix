a533393133938727a83873a0ef7838cfe6f850bc
infraflakes <infraflakes@proton.me>
2025-11-06 15:58:12 +0700

    Final restructure

--- home/environment/shared/shared.nix
+++ home/environment/shared/shared.nix
@@ -10,15 +10,22 @@
     ./cli/fish.nix
     ./cli/zsh.nix
     ./cli/kitty.nix
+    ./cli/starship.nix
+    ./cli/fastfetch.nix
 
     ./scripts/scripts.nix
 
     ./sys/autostart.nix
-    ./sys/env.nix
     ./sys/ime.nix
     ./sys/modify-desktop-entry.nix
     ./sys/uwsm.nix
 
-    ./theme/theme.nix
+    ./shell/fonts.nix
+    ./shell/gtk.nix
+    ./shell/mako.nix
+    ./shell/rofi.nix
+    ./shell/swaylock.nix
+    ./shell/wallpapers.nix
+    ./shell/waybar.nix
   ];
 }
