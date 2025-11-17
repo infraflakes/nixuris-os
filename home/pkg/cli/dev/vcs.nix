6894f6967c69ab841584a5a524ba78fccac4a72c
infraflakes <infraflakes@proton.me>
2025-11-17 18:25:22 +0700

    diff-so-fancy

--- home/pkg/cli/dev/vcs.nix
+++ home/pkg/cli/dev/vcs.nix
@@ -6,11 +6,23 @@
   programs.direnv = {
     enable = true;
   };
+  programs.diff-so-fancy = {
+    enable = true;
+    enableGitIntegration = true;
+  };
   programs.git = {
     enable = true;
-    settings.user = {
-      email = "infraflakes@proton.me";
-      name = "infraflakes";
+    settings = {
+      user = {
+        email = "infraflakes@proton.me";
+        name = "infraflakes";
+      };
+      color.diff = {
+        meta = "blue";
+        frag = "brightyellow";
+        old = "red";
+        new = "green";
+      };
     };
   };
   programs.jujutsu = {
